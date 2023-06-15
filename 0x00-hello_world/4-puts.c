f9397e9d6f15c87f307898cb986d0b53b32ea987
Ridhu4 <rudhwanbashir@gmail.com>
2023-06-15 15:53:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints code followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"
+	    );
+	return (0);
+}
