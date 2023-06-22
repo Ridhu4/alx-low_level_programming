0302b33720d17d02a6ce594c9dba855c914c2952
Ridhu4 <rudhwanbashir@gmail.com>
2023-06-22 12:30:08 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,13 @@
 #include "main.h"
 /**
- * main - Print "main.h" followed by a new line.
- * Description: You are not allowed to include standard libraries.
+ * main - Test function for positive or negative
  * Return: 0
  */
 int main(void)
 {
-	char main[] = "main";
-	int i = 0;
-
-	while (holberton[i] != '\0')
-	{
-		_putchar(main[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+ int i;
+ i = 0;
+ positive_or_negative(i);
+return (0);
 }
+
