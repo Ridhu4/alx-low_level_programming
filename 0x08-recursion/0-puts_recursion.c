bff76e01291b962aff292b87494979b812cab198
Ridhu4 <rudhwanbashir@gmail.com>
2023-07-05 13:59:51 -0700

    2-strlen_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion -function like puts();
+ * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int ((((((((char *s))))));
-({
+void _puts_recursion(char *s)
+{
 if (*s)
 {
 _putchar(*s);
 _puts_recursion(s + 1);
 }
 else
-_putchar(*n *);
+_putchar('\n');
 }
